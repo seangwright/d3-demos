da8bd2ee19d46e2499038cf7dceb972c6defd440
Sean Wright <s.galen.wright@gmail.com>
2015-02-16 23:41:53 -0500

    Replaced svg width/height with variables, added transformed x-axis

--- script/main.js
+++ script/main.js
@@ -2,28 +2,56 @@
     'use strict';
 
     document.addEventListener("DOMContentLoaded", function (event) {
-        var dataArray = [20, 40, 60];
+        var dataArray = [10, 20, 30, 40, 50, 60];
 
-        var width = 500,
-            height = 500;
+        var max = 60;
+        
+        var numTicks = 2;
+        
+        var containerWidth = 500,
+            containerHeight = 500,
+            width = 400,
+            height = 400;
 
         var widthScale = d3
             .scale
             .linear()
-            .domain([0, 60])
+            .domain([0, max])
             .range([0, width]);
 
         var colorScale = d3
             .scale
             .linear()
-            .domain([9, 60])
+            .domain([9, max])
             .range(["red", "blue"]);
+
+        var axis = d3
+            .svg
+            .axis()
+            .scale(widthScale)
+            .ticks(numTicks)
+            .tickValues(function () {
+                var vals = [0];
+                
+                for (var x = 1; x <= numTicks; x += 1) {
+                    vals.push(max/x);
+                }
+                
+                return vals;
+            });
 
         var canvas = d3
             .select("body")
             .append("svg")
-            .attr("width", width)
-            .attr("height", height);
+            .attr("width", containerWidth)
+            .attr("height", containerHeight)
+            .append("g")
+            .attr("transform", "translate(10, 10)")
+            
+        canvas
+            .append("g")
+            .attr("transform", "translate(0, 400)")
+            .call(axis);
 
         var bars = canvas
             .selectAll("rect")
@@ -39,7 +67,7 @@
                 return i * 60;
             })
             .attr("fill", function (d) {
-                return colorScale(d);   
+                return colorScale(d);
             });
     });
 }());
