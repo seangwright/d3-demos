5569a945456228a8515f15972098b9f4c96b5a11
Sean Wright <s.galen.wright@gmail.com>
2015-02-16 23:08:34 -0500

    Added data, scales, canvas and rectangles

--- script/main.js
+++ script/main.js
@@ -2,6 +2,44 @@
     'use strict';
 
     document.addEventListener("DOMContentLoaded", function (event) {
-        
+        var dataArray = [20, 40, 60];
+
+        var width = 500,
+            height = 500;
+
+        var widthScale = d3
+            .scale
+            .linear()
+            .domain([0, 60])
+            .range([0, width]);
+
+        var colorScale = d3
+            .scale
+            .linear()
+            .domain([9, 60])
+            .range(["red", "blue"]);
+
+        var canvas = d3
+            .select("body")
+            .append("svg")
+            .attr("width", width)
+            .attr("height", height);
+
+        var bars = canvas
+            .selectAll("rect")
+            .data(dataArray)
+            .enter()
+            .append("rect")
+            .attr("width", function (d) {
+                return widthScale(d);
+            })
+            .attr("height", 50)
+            .attr("x", 0)
+            .attr("y", function (d, i) {
+                return i * 60;
+            })
+            .attr("fill", function (d) {
+                return colorScale(d);   
+            });
     });
 }());
